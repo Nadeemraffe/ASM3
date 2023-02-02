05daab5fb081aacfaa6fdcbf846c040ec178aa5d
Nadeemraffe <123829445+Nadeemraffe@users.noreply.github.com>
2023-02-02 05:31:08 -0800

    Add files via upload

---
+++ ASM3/app.js
@@ -0,0 +1,11 @@
+// Question No1
+// var num="I Am 15 Year old"
+// alert(num)
+// Question No2
+// var time="You have visited site 14 Times."
+// alert(time)
+// Question No3
+// var a="03-May-2003"
+// document.write(a)
+// Question No4
+document.write("<b>John Doe</b> Ordered <b>5 T-Shirt(s)</b> on XYZ Clothing Store")
